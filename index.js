06cbd0de07c402e02e402b2636aa2d5fa635ff72
Cody-Wise <cody@sharptackmedia.com>
2022-07-11 12:04:05 -0700

    Finished shift and test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = { push, pop, unshift };
+module.exports = { push, pop, unshift, shift };
 
 function push(arr, item) {
   arr[arr.length] = item;
@@ -13,7 +13,7 @@ function pop(arr, item) {
 
 //i = arr.length - 1; i >= 0; i--
 function unshift(arr, item) {
-  for (i = arr.length; i >= 0; i--) {
+  for (i = arr.length; i > 0; i--) {
     arr[i] = arr[i - 1]; // index of the array is equal to array[i + 1]
     arr[0] = item; // first item in array should be item we pass down, then increment i by one
     return arr.length;
@@ -21,4 +21,14 @@ function unshift(arr, item) {
   //for loop that iterates through each item in the array
   //update/shift the index up one
   //change the length of the array
+}
+
+//i = arr.length - 1; i >= 0; i--
+function shift(arr, item) {
+  const firstItem = arr[0];
+  for (i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr.length = arr.length - 1;
+  return firstItem;
 }
--- index.test.js
+++ index.test.js
@@ -1,28 +1,37 @@
-const { push, pop, unshift } = require("./index.js");
+const { push, pop, unshift, shift } = require('./index.js');
 
-describe("push", () => {
-  it("should add an element to an array", () => {
-    const arr = ["a", "b", "c"];
-    expect(push(arr, "d")).toBe(4);
+describe('push', () => {
+  it('should add an element to an array', () => {
+    const arr = ['a', 'b', 'c'];
+    expect(push(arr, 'd')).toBe(4);
     expect(arr.length).toBe(4);
-    expect(arr[3]).toBe("d");
+    expect(arr[3]).toBe('d');
   });
 });
 
-describe("push", () => {
-  it("should remove last element from array", () => {
-    const arr = ["a", "b", "c"];
-    expect(pop(arr)).toBe("c");
+describe('push', () => {
+  it('should remove last element from array', () => {
+    const arr = ['a', 'b', 'c'];
+    expect(pop(arr)).toBe('c');
     expect(arr.length).toBe(2);
-    expect(arr[1]).toBe("b");
+    expect(arr[1]).toBe('b');
   });
 });
 
-describe("unshift", () => {
-  it("add item to the beginning of the array", () => {
-    const arr = ["a", "b", "c"];
-    expect(unshift(arr, "d")).toBe(4);
+describe('unshift', () => {
+  it('add item to the beginning of the array', () => {
+    const arr = ['a', 'b', 'c'];
+    expect(unshift(arr, 'd')).toBe(4);
     expect(arr.length).toBe(4);
-    expect(arr[0]).toBe("d");
+    expect(arr[0]).toBe('d');
+  });
+});
+
+describe('shift', () => {
+  it('remove item from the beginning of the array', () => {
+    const arr = ['a', 'b', 'c'];
+    expect(shift(arr)).toBe('a');
+    expect(arr.length).toBe(2);
+    expect(arr[0]).toBe('b');
   });
 });
