eb3e78de9169d9c681ac6e9f22b225092ce69ba0
Cody-Wise <cody@sharptackmedia.com>
2022-07-11 10:24:50 -0700

    Added pop exercise  and test

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-module.exports = { push };
+module.exports = { push, pop };
 
 function push(arr, item) {
   arr[arr.length] = item;
   return arr.length;
+}
+
+function pop(arr, item) {
+  const lastitem = arr[arr.length - 1];
+  arr.length = arr.length - 1;
+  return lastitem;
 }
